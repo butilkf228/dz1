291ab1cc9a653760a9c3077964f6e21e3844d7dd
butilkf228 <129097758+butilkf228@users.noreply.github.com>
2024-01-22 17:39:55 +0100

    d

---
+++ dz2/script.js
@@ -0,0 +1,53 @@
+const celsius = -79;
+const ferenheit = -110.2;
+
+console.log(ferenheit);
+
+const daysInMonth = 31;
+
+const hoursInMonth = daysInMonth * 24;
+const minutesInMonth = hoursInMonth * 60;
+
+console.log(hoursInMonth);
+console.log(minutesInMonth);
+
+let hp = 100;
+let energy = 100;
+
+const hpLowered = hp - 50;
+const energyLowered = energy - 60;
+
+console.log(hpLowered);
+console.log(energyLowered);
+
+const price = 100;
+const discount = price * 0.10;
+
+console.log(discount);
+
+const number = 10.5;
+const rounded = Math.floor(number);
+
+console.log(rounded);
+
+const num = "5.35";
+const float = parseFloat(num);
+
+console.log(float);
+
+const num2 = "10";
+const int = parseInt(num2);
+
+console.log(num2);
+
+const num3 = 20;
+const sq = Math.sqrt(num3);
+
+const fullNum = 10;
+const textNum = "10";
+
+const parsed = toString(fullNum);
+const stringed = parseInt(textNum);
+
+console.log(parsed);
+console.log(stringed);
